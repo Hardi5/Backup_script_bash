0f2e8c6588ed723709705564ec7ec69abbb3bfb5
Hardi <hardi.kakalaw@chasacademy.se>
2023-04-13 15:09:11 +0200

    some changes

--- backup.sh
+++ backup.sh
@@ -17,22 +17,19 @@ if [ ! -d "$TARGET_DIR" ]; then
 
     # Create remote directory if it doesnt already exist
 
-    ssh "$REMOTE_USER@$REMOTE_HOST" "mkdir -p /"$REMOTE_DIR/$TIMESTAMP""
+    ssh "$REMOTE_USER@$REMOTE_HOST" "mkdir -p /"$REMOTE_DIR/"$TIMESTAMP"""
 
     # Backup from source directory to remote location
-    rsync -avz  /$SOURCE_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/$TIMESTAMP/
+    rsync -avz  /$SOURCE_DIR $REMOTE_DIR/"$TIMESTAMP"/
 
 else 
     echo "Target exists locally, backing up"
 
-
-    # Timestamp directory for local destination and then join it with target to equal backup_dir
-    TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
     BACKUP_DIR="$TARGET_DIR/$TIMESTAMP"
 
     # Create the local dir. with timestamp
     mkdir -p "$BACKUP_DIR"
 
     # backup from source dir to local target dir
-    rsync -avz  "$SOURCE_DIR" "$BACKUP_DIR"
+    rsync -avz  "$SOURCE_DIR/" "$BACKUP_DIR/"
 fi
